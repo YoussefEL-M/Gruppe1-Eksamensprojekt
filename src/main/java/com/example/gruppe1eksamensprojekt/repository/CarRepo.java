5ef0fdf71471d2bf124c6f0c7f940ab0fa60fbae
CVS2003 <clara.vej@outlook.com>
2024-05-09 19:11:10 +0200

    Indsat ny tabel report og ændret i antal tegn i varchar

--- src/main/java/com/example/gruppe1eksamensprojekt/repository/CarRepo.java
+++ src/main/java/com/example/gruppe1eksamensprojekt/repository/CarRepo.java
@@ -16,27 +16,27 @@ public class CarRepo {
     private JdbcTemplate jdbcTemplate;
 
     public void create(Car car) {
-        String sql = "INSERT INTO cars (id, brand, serialNumber, model, color, trimLevel, steelPrice, registrationTax, emission, damaged, ds, licensePlate) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO car (id, brand, serialNumber, model, color, trimLevel, steelPrice, registrationTax, emission, damaged, ds, licensePlate) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
         jdbcTemplate.update(sql, car.getId(), car.getBrand(), car.getSerialNumber(), car.getModel(), car.getColor(), car.getTrimLevel(), car.getSteelPrice(), car.getRegistrationTax(), car.getEmission(), car.isDamaged(), car.isDs(), car.getLicensePlate());
     }
 
-    public Car getCareByID(int id){
-        String sql = "SELECT * FROM cars WHERE id = ?";
+    public Car getCarById(int id){
+        String sql = "SELECT * FROM car WHERE id = ?";
         RowMapper<Car> rowMapper = new BeanPropertyRowMapper<>(Car.class);
         return jdbcTemplate.queryForObject(sql, rowMapper, id);
     }
 
     public void update(Car car) {
-        String sql = "UPDATE cars SET brand=?, serialNumber=?, model=?, color=?, trimLevel=?, steelPrice=?, registrationTax=?, emission=?, damaged=?, ds=?, licensePlate=? WHERE id=?";
+        String sql = "UPDATE car SET brand=?, serialNumber=?, model=?, color=?, trimLevel=?, steelPrice=?, registrationTax=?, emission=?, damaged=?, ds=?, licensePlate=? WHERE id=?";
         jdbcTemplate.update(sql, car.getBrand(), car.getSerialNumber(), car.getModel(), car.getColor(), car.getTrimLevel(), car.getSteelPrice(), car.getRegistrationTax(), car.getEmission(), car.isDamaged(), car.isDs(), car.getLicensePlate(), car.getId());
     }
     public void delete(int id) {
-        String sql = "DELETE FROM cars WHERE id = ?";
+        String sql = "DELETE FROM car WHERE id = ?";
         jdbcTemplate.update(sql, id);
     }
 
     public List<Car> getAll() {
-        String sql = "SELECT * FROM cars";
+        String sql = "SELECT * FROM car";
         RowMapper<Car> rowMapper = new BeanPropertyRowMapper<>(Car.class);
         return jdbcTemplate.query(sql, rowMapper);
     }
--- src/main/resources/static/sql/car_rental_gruppe1.sql
+++ src/main/resources/static/sql/car_rental_gruppe1.sql
@@ -1,50 +1,61 @@
+/* Clara */
+
 CREATE DATABASE IF NOT EXISTS car_rental_gruppe1;
 
 USE car_rental_gruppe1;
 
+
 CREATE TABLE IF NOT EXISTS customer
 (id 	INT	PRIMARY KEY	NOT NULL	AUTO_INCREMENT,
- name	VARCHAR(250)	NOT NULL,
-    telnr	VARCHAR(250)	NOT NULL,
-    email	VARCHAR(250),
-    address	VARCHAR(250)	NOT NULL,
+ name	VARCHAR(100)	NOT NULL,
+    telnr	VARCHAR(50)	NOT NULL,
+    email	VARCHAR(50),
+    address	VARCHAR(150)	NOT NULL,
     birthDate	DATE	NOT NULL
     );
 
 CREATE TABLE IF NOT EXISTS rental
 (id	INT	PRIMARY KEY	NOT NULL	AUTO_INCREMENT,
- pickUpLocation	VARCHAR(250)	NOT NULL,
-    returnLocation	VARCHAR(250)	NOT NULL,
-    type	VARCHAR(250)	NOT NULL,
+ pickUpLocation	VARCHAR(150)	NOT NULL,
+    returnLocation	VARCHAR(150)	NOT NULL,
+    type	VARCHAR(100)	NOT NULL,
     startDate	DATE	NOT NULL,
     endDate	DATE	NOT NULL
     );
 
-CREATE TABLE IF NOT EXISTS caridentification
-(serialNumber VARCHAR(250) PRIMARY KEY	NOT NULL,
-    brand	VARCHAR(250)	NOT NULL,
-    model	VARCHAR(250)	NOT NULL
+CREATE TABLE IF NOT EXISTS report
+(id	INT PRIMARY KEY	NOT NULL	AUTO_INCREMENT,
+ rentalId	INT,	FOREIGN KEY(rentalId)	REFERENCES rental(id),
+    title	VARCHAR(50)	NOT NULL,
+    date	DATE	NOT NULL
     );
 
+
+CREATE TABLE IF NOT EXISTS carIdentification
+(serialNumber VARCHAR(50) PRIMARY KEY	NOT NULL,
+    brand	VARCHAR(50)	NOT NULL,
+    model	VARCHAR(50)	NOT NULL
+    );
+
+
 CREATE TABLE IF NOT EXISTS car
-(id	INT	PRIMARY KEY	NOT NULL	AUTO_INCREMENT,
- serialNumber	VARCHAR(250),	FOREIGN KEY(serialNumber)	REFERENCES caridentification(serialNumber),
-    color	VARCHAR(250)	NOT NULL,
-    trimLevel	VARCHAR(250)	NOT NULL,
+(id	INT	PRIMARY KEY	NOT NULL,
+ serialNumber	VARCHAR(50),	FOREIGN KEY(serialNumber)	REFERENCES caridentification(serialNumber),
+    color	VARCHAR(50)	NOT NULL,
+    trimLevel	VARCHAR(100)	NOT NULL,
     steelPrice	DOUBLE,
     registrationTax	DOUBLE	NOT NULL,
     emission	DOUBLE,
     damaged	BOOLEAN	NOT NULL,
     ds	BOOLEAN	NOT NULL,
-    licensePlate	VARCHAR(250)	NOT NULL
+    licensePlate	VARCHAR(50)	NOT NULL
     );
-
 
 
 CREATE TABLE IF NOT EXISTS user
 (id	INT	PRIMARY KEY	NOT NULL	AUTO_INCREMENT,
- name	VARCHAR(250)	NOT NULL,
-    username	VARCHAR(250)	NOT NULL,
-    password	VARCHAR(250)	NOT NULL,
-    email	VARCHAR(250)	NOT NULL
+ name	VARCHAR(150)	NOT NULL,
+    username	VARCHAR(50)	NOT NULL,
+    password	VARCHAR(50)	NOT NULL,
+    email	VARCHAR(50)	NOT NULL
     );
