322df041430c3fd4d37368926c03da0e158ecc97
CVS2003 <clara.vej@outlook.com>
2024-05-14 10:46:43 +0200

    Opdateret sql med tildeling af rettigheder i sql og UserRepo.

--- src/main/resources/static/sql/car_rental_gruppe1.sql
+++ src/main/resources/static/sql/car_rental_gruppe1.sql
@@ -4,62 +4,66 @@ CREATE DATABASE IF NOT EXISTS car_rental_gruppe1;
 
 USE car_rental_gruppe1;
 
+CREATE USER 'root''%' IDENTIFIED BY 'Integer_9';
+
+GRANT ALL ON car_rental_gruppe1.* TO 'root''%' WITH GRANT OPTION;
 
 CREATE TABLE IF NOT EXISTS customer
 (id 	INT	PRIMARY KEY	NOT NULL	AUTO_INCREMENT,
  name	VARCHAR(100)	NOT NULL,
-    telnr	VARCHAR(50)	NOT NULL,
-    email	VARCHAR(50),
-    address	VARCHAR(150)	NOT NULL,
-    birthDate	DATE	NOT NULL
-    );
+ telnr	VARCHAR(50)	NOT NULL,
+ email	VARCHAR(50),
+ address	VARCHAR(150)	NOT NULL,
+ birthDate	DATE	NOT NULL
+);
 
 
 CREATE TABLE IF NOT EXISTS carIdentification
 (serialNumber VARCHAR(50) PRIMARY KEY	NOT NULL,
-    brand	VARCHAR(50)	NOT NULL,
-    model	VARCHAR(50)	NOT NULL
-    );
+ brand	VARCHAR(50)	NOT NULL,
+ model	VARCHAR(50)	NOT NULL
+);
 
 
 CREATE TABLE IF NOT EXISTS car
 (id	INT	PRIMARY KEY	NOT NULL,
  serialNumber	VARCHAR(50),	FOREIGN KEY(serialNumber)	REFERENCES caridentification(serialNumber),
-    color	VARCHAR(50)	NOT NULL,
-    trimLevel	VARCHAR(100)	NOT NULL,
-    steelPrice	DOUBLE,
-    registrationTax	DOUBLE	NOT NULL,
-    emission	DOUBLE,
-    damaged	BOOLEAN	NOT NULL,
-    ds	BOOLEAN	NOT NULL,
-    licensePlate	VARCHAR(50)	NOT NULL
-    );
+ color	VARCHAR(50)	NOT NULL,
+ trimLevel	VARCHAR(100)	NOT NULL,
+ steelPrice	DOUBLE,
+ registrationTax	DOUBLE	NOT NULL,
+ emission	DOUBLE,
+ damaged	BOOLEAN	NOT NULL,
+ ds	BOOLEAN	NOT NULL,
+ licensePlate	VARCHAR(50)	NOT NULL
+);
 
 
 CREATE TABLE IF NOT EXISTS user
 (id	INT	PRIMARY KEY	NOT NULL	AUTO_INCREMENT,
- type   VARCHAR(100)    NOT NULL,
+ type	VARCHAR(100)	NOT NULL,
  name	VARCHAR(150)	NOT NULL,
-    username	VARCHAR(50)	NOT NULL,
-    password	VARCHAR(50)	NOT NULL,
-    email	VARCHAR(50)	NOT NULL
-    );
+ username	VARCHAR(50)	NOT NULL,
+ password	VARCHAR(50)	NOT NULL,
+ email	VARCHAR(50)	NOT NULL
+);
 
 CREATE TABLE IF NOT EXISTS rental
 (id	INT	PRIMARY KEY	NOT NULL	AUTO_INCREMENT,
  customer_id	INT, FOREIGN KEY(customer_id)	REFERENCES customer(id),
-    car_id	INT, FOREIGN KEY(car_id)	REFERENCES car(id),
-    user_id	INT, FOREIGN KEY(user_id)	REFERENCES	user(id),
-    pickUpLocation	VARCHAR(150)	NOT NULL,
-    returnLocation	VARCHAR(150)	NOT NULL,
-    type	VARCHAR(100)	NOT NULL,
-    startDate	DATE	NOT NULL,
-    endDate	DATE	NOT NULL
-    );
+ car_id	INT, FOREIGN KEY(car_id)	REFERENCES car(id),
+ user_id	INT, FOREIGN KEY(user_id)	REFERENCES	user(id),
+ pickUpLocation	VARCHAR(150)	NOT NULL,
+ returnLocation	VARCHAR(150)	NOT NULL,
+ type	VARCHAR(100)	NOT NULL,
+ startDate	DATE	NOT NULL,
+ endDate	DATE	NOT NULL
+);
 
 CREATE TABLE IF NOT EXISTS report
 (id	INT PRIMARY KEY	NOT NULL	AUTO_INCREMENT,
  rentalId	INT,	FOREIGN KEY(rentalId)	REFERENCES rental(id),
-    title	VARCHAR(50)	NOT NULL,
-    date	DATE	NOT NULL
-    );
+ title	VARCHAR(50)	NOT NULL,
+ date	DATE	NOT NULL,
+ comment	VARCHAR(500)	NOT NULL
+);
