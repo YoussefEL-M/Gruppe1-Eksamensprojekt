78ba5ebf2e6cdd5c5da8b30c2e04e3992f0aea2a
bjli0001 <bjli0001@stud.kea.dk>
2024-05-20 09:31:41 +0200

    start på submitReport service metode

--- src/main/java/com/example/gruppe1eksamensprojekt/service/ReportService.java
+++ src/main/java/com/example/gruppe1eksamensprojekt/service/ReportService.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
@@ -59,5 +61,67 @@ public class ReportService { // Severin
     }
 
     public int lastId(){return reportRepo.lastId(); }
+
+
+    public String submitReport(int idForRental, String reportTitle, LocalDate reportDate, String treatment, String comment, String report0damage, String report1damage, String report2damage, String report3damage, String report4damage,  int report0price,   int report1price,  int report2price,  int report3price,  int report4price, RedirectAttributes redirectAttributes){
+        boolean error = false;
+        int customerId = 0;
+        int carId = 0;
+
+        try{
+            customerId = Integer.parseInt(customer.split("\\.")[0]);
+            customerRepo.getCustomerById(customerId);
+        } catch (EmptyResultDataAccessException | NumberFormatException E){
+            redirectAttributes.addFlashAttribute("customerNotFound", true);
+            error = true;
+        }
+        // Kan der være behov for at registrere en kontrakt efter lejeperioden begynder?
+        if(LocalDate.parse(startDate).isBefore(LocalDate.now())) {
+            redirectAttributes.addFlashAttribute("timeTravelException", true);
+            error = true;
+        }
+
+
+        try{
+            carId = Integer.parseInt(car.split("\\.")[0]);
+            if(!carRepo.getCarById(carId).getStatus().equals(CarStatus.AVAILABLE)){
+                redirectAttributes.addFlashAttribute("carAlreadyRented", true);
+                error = true;
+            }
+        } catch (EmptyResultDataAccessException | NumberFormatException E){
+            redirectAttributes.addFlashAttribute("carNotFound", true);
+            error = true;
+        }
+
+        // Hvis type ikke er Limited, sæt type lig antal måneder valgt til Unlimited.
+        if(!type.equals("5")) type = unlimitedMonth;
+        if(type.isEmpty()) type = "0";
+        if(3>Integer.parseInt(type)||Integer.parseInt(type)>36) {
+            redirectAttributes.addFlashAttribute("typeError", true);
+            error = true;
+        }
+
+
+        if(error) {
+            redirectAttributes.addFlashAttribute("customer", customer);
+            redirectAttributes.addFlashAttribute("startDate", startDate);
+            redirectAttributes.addFlashAttribute("pickuppoint", pickuppoint);
+            redirectAttributes.addFlashAttribute("car", car);
+            redirectAttributes.addFlashAttribute("type", Integer.parseInt(type));
+            redirectAttributes.addFlashAttribute("dropoffpoint", dropoffpoint);
+            redirectAttributes.addFlashAttribute("unlimitedMonth", unlimitedMonth);
+        } else{
+            String endDate = calcEndDate(startDate,type);
+            Car carToUpdate = carRepo.getCarById(carId);
+            carToUpdate.setStatus(CarStatus.RENTED);
+            carRepo.update(carToUpdate);
+            Rental rental = new Rental(pickuppoint, dropoffpoint, type, customerId, startDate, endDate, carId, false);
+            rentalRepo.create(rental);
+            return "redirect:/rental";
+        }
+
+        return "redirect:/createRental";
+    }
+
 
 }
