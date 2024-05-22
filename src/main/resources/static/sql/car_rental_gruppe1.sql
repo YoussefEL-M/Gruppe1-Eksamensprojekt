1bcd88c3e1df47ba7880f09065be3e2d8d63d4af
bjli0001 <bjli0001@stud.kea.dk>
2024-05-22 09:06:23 +0200

    rettet i inserts i sql

--- src/main/resources/static/sql/car_rental_gruppe1.sql
+++ src/main/resources/static/sql/car_rental_gruppe1.sql
@@ -103,15 +103,15 @@ INSERT INTO carIdentification (serialNumber, brand, model) VALUES ('0000', 'Ford
 INSERT INTO carIdentification (serialNumber, brand, model) VALUES ('1111', 'Ford', 'Ka');
 INSERT INTO carIdentification (serialNumber, brand, model) VALUES ('2222', 'Tesla', 'Cypertruck');
 
-INSERT INTO car (id, serialNumber, color, trimLevel, steelPrice, registrationTax, emission, status, ds, licensePlate, fuelType, kmTraveled, fuelEfficiency, price, manual, lastUpdated) VALUES ('1','1234','Red', 'Max', '14', '1000','30', 'PENDING', 1, 'Kachow', 'PETROL', '100', '20', '100000',  '0', '2024-05-15');
-INSERT INTO car (id, serialNumber, color, trimLevel, steelPrice, registrationTax, emission, status, ds, licensePlate, fuelType, kmTraveled, fuelEfficiency, price, manual, lastUpdated) VALUES ('2','0000','brown', 'Max', '14', '1000','30', 'RENTED', 1, 'Kachow', 'PETROL', '100', '20', '100',  '0', '2024-05-17');
-INSERT INTO car (id, serialNumber, color, trimLevel, steelPrice, registrationTax, emission, status, ds, licensePlate, fuelType, kmTraveled, fuelEfficiency, price, manual, lastUpdated) VALUES ('3','1111','Red', 'Max', '14', '1000','30', 'DAMAGED', 1, 'Kachow', 'PETROL', '100', '20', '100000',  '0', '2024-05-16');
-INSERT INTO car (id, serialNumber, color, trimLevel, steelPrice, registrationTax, emission, status, ds, licensePlate, fuelType, kmTraveled, fuelEfficiency, price, manual, lastUpdated) VALUES ('4','2222','brown', 'Max', '14', '1000','30', 'DAMAGED', 1, 'Kachow', 'ELECTRIC', '100', '20', '100',  '0', '2024-05-15');
+INSERT INTO car (id, serialNumber, color, trimLevel, steelPrice, registrationTax, emission, status, ds, licensePlate, fuelType, kmTraveled, fuelEfficiency, price, manual, lastUpdated) VALUES ('1','1234','Red', 'Max', '14', '1000','30', 'AVAILABLE', 1, 'Kachow', 'PETROL', '100', '20', '100000',  '0', '2024-05-15');
+INSERT INTO car (id, serialNumber, color, trimLevel, steelPrice, registrationTax, emission, status, ds, licensePlate, fuelType, kmTraveled, fuelEfficiency, price, manual, lastUpdated) VALUES ('2','0000','brown', 'Max', '14', '1000','30', 'AVAILABLE', 1, 'Kachow', 'PETROL', '100', '20', '100',  '0', '2024-05-17');
+INSERT INTO car (id, serialNumber, color, trimLevel, steelPrice, registrationTax, emission, status, ds, licensePlate, fuelType, kmTraveled, fuelEfficiency, price, manual, lastUpdated) VALUES ('3','1111','Red', 'Max', '14', '1000','30', 'AVAILABLE', 1, 'Kachow', 'PETROL', '100', '20', '100000',  '0', '2024-05-16');
+INSERT INTO car (id, serialNumber, color, trimLevel, steelPrice, registrationTax, emission, status, ds, licensePlate, fuelType, kmTraveled, fuelEfficiency, price, manual, lastUpdated) VALUES ('4','2222','brown', 'Max', '14', '1000','30', 'AVAILABLE', 1, 'Kachow', 'ELECTRIC', '100', '20', '100',  '0', '2024-05-15');
 
 INSERT INTO USER (type, name, username, password, email) VALUES ('Dataregistrering', 'lars', 'lars', 'lars', 'lars@lars.dk');
 INSERT INTO USER (type, name, username, password, email) VALUES ('SkadeOgUdbedring', 's', 's', 's', 's@s.dk');
 
-INSERT INTO rental (user_id, pickUpLocation, returnLocation, type, customer_id, startDate, endDate, car_id,status) VALUES ('1','a', 'a', '3', '1', '2024-01-01', '2024-04-01', '1','FRESH');
+
 
 select * from car;
 select * from rental;
