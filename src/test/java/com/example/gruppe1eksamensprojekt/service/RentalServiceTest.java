c9b7c7714d2f1bfdc47bf0835fc32655cb186b78
YoussefEL-M <yoel0001@stud.kea.dk>
2024-05-22 11:06:09 +0200

    Test commitet

---
+++ src/test/java/com/example/gruppe1eksamensprojekt/service/RentalServiceTest.java
@@ -0,0 +1,13 @@
+package com.example.gruppe1eksamensprojekt.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RentalServiceTest {
+
+    @Test
+    void calcEndDate() {
+
+    }
+}
